873e46f38b637d6997c9fbebcf99d0a525d9c186
NadiaAfshar <saxystar@aol.com>
2014-02-21 22:25:54 -0500

    Initial Commit

---
+++ main.lua
@@ -0,0 +1,45 @@
+function love.load()
+  
+  Tileset = love.graphics.newImage('sampletile.png')
+  
+  tileW, tileH = 32,32
+  tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
+  
+  TileTable = {
+  { 4,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,4 },
+  { 4,1,3,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,4 },
+  { 4,1,1,1,1,1,1,1,2,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+  { 4,1,1,4,1,1,1,1,1,2,2,1,1,4,1,1,1,4,1,4,2,2,1,1,4 },
+  { 4,1,1,4,1,1,1,1,4,3,3,4,1,2,1,1,1,2,1,4,1,1,1,1,4 },
+  { 4,1,1,4,1,1,1,1,4,3,3,4,1,1,4,1,4,1,1,4,2,2,1,1,4 },
+  { 4,1,1,4,1,1,1,1,4,3,3,4,1,1,2,1,2,1,1,4,1,1,1,1,4 },           
+  { 4,1,1,4,1,1,1,1,2,3,3,2,1,1,1,4,1,1,1,4,1,1,1,1,4 },
+  { 4,1,1,2,2,2,2,1,1,2,2,1,1,1,1,2,1,3,1,2,2,2,1,1,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+  { 4,1,1,1,1,1,1,1,1,2,1,1,1,1,2,2,4,1,1,1,1,1,1,1,4 },
+  { 4,1,1,1,1,1,1,1,4,3,4,1,1,1,1,1,2,1,1,1,1,1,1,1,4 },
+  { 4,1,1,3,1,1,1,1,2,3,2,1,1,1,1,2,1,1,1,1,1,1,1,1,4 },
+  { 4,1,1,1,1,1,1,1,1,2,1,1,2,1,2,1,1,1,1,1,1,1,3,1,4 },
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
+  { 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2 }
+}
+  
+ Quads = {}
+  Quads[1] = love.graphics.newQuad(0,  0, tileW, tileH, tilesetW, tilesetH) -- 1 = Blue
+  Quads[2] = love.graphics.newQuad(32, 0, tileW, tileH, tilesetW, tilesetH) -- 2 = Pink
+  Quads[3] = love.graphics.newQuad(0, 32, tileW, tileH, tilesetW, tilesetH) -- 3 = Purple
+  Quads[4] = love.graphics.newQuad(32, 32, tileW, tileH, tilesetW, tilesetH) -- 4 = Green 
+ 
+end
+
+function love.draw()
+for rowIndex=1, #TileTable do
+    local row = TileTable[rowIndex]
+    for columnIndex=1, #row do
+      local number = row[columnIndex]
+      love.graphics.draw(Tileset, Quads[number], (columnIndex-1)*tileW, (rowIndex-1)*tileH)
+    end
+  end
+end
